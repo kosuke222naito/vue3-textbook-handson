9975f8d28c6864aadfa7167bcac8b1892bf62055
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-04-28 16:05:56 +0900

    add project

---
+++ chap4/directive-bind-basic/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const url = ref('https://vuejs.org/')
+</script>
+
+<template>
+  <p><a :href="url" target="_blank">Vueのサイト</a></p>
+  <p><a :href="url + 'guide/introduction.html'" target="_blank">Vueのガイドページ</a></p>
+</template>

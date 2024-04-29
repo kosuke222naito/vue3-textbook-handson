ea4bd658c890e6117a8fcbeaec9dd1e56504990d
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-04-29 16:54:09 +0900

    add project

---
+++ chap4/directive-class/src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const message = ref("Hello World!");
+const isMessageColorRed = ref(true);
+const isBgColorBlack = ref(false);
+
+const messageStyle = ref({
+  isMessageColorRed: false,
+  isBgColorBlack: true,
+});
+
+const computedStyle = computed(
+  (): {
+    textColorRed: boolean;
+    bgColorBlack: boolean;
+  } => {
+    return {
+      textColorRed: !!Math.round(Math.random()),
+      bgColorBlack: !!Math.round(Math.random()),
+    };
+  },
+);
+</script>
+
+<template>
+  <p :class="{ textColorRed: true, isBgColorBlack: true }">{{ message }}</p>
+  <p :class="{ textColorRed: isMessageColorRed, isBgColorBlack: isBgColorBlack }">{{ message }}</p>
+  <p :class="{ textColorPink: true }">{{ message }}</p>
+  <p :class="{ 'text-color-pink': true }">{{ message }}</p>
+  <p
+    class="textColorPink"
+    :class="{ textColorRed: isMessageColorRed, bgColorBlack: isBgColorBlack }"
+  >
+    {{ message }}
+  </p>
+  <p class="textSize24px" :class="messageStyle">{{ message }}</p>
+  <p :class="computedStyle">{{ message }}</p>
+</template>
+
+<style>
+.textColorRed {
+  color: red;
+}
+
+.text-color-pink {
+  color: pink;
+}
+
+.bgColorBlack {
+  background-color: black;
+}
+
+.textSize24px {
+  font-size: 24px;
+}
+</style>

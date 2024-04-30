6e417cf96506b34ab70c7afe1e0d1c388cf2b972
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-04-30 21:39:28 +0900

    add project

---
+++ chap4/directive-on-param-both/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref("イベント前(ここをクリック！)");
+const bgColorEvent = ref("white");
+
+const onClickWithEvent = (bgColor: string, event: MouseEvent): void => {
+  bgColorEvent.value = bgColor;
+  message.value = event.timeStamp.toString();
+};
+</script>
+
+<template>
+  <p @click="onClickWithEvent('green', $event)" :style="{ backgroundColor: 'bgColorEvent' }">
+    {{ message }}
+  </p>
+</template>

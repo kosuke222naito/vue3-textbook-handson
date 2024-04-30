4079389101c301ebda83ad6ecf6d74ef6bb12cb9
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-04-30 23:09:18 +0900

    add project

---
+++ chap4/directive-on-modifiers/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref("まだです");
+const onEnterKeyPress = (): void => {
+  message.value = "エンターが押されました";
+};
+const onRightButtonClick = (): void => {
+  message.value = "右クリックされました";
+};
+const onClickWithShift = (): void => {
+  message.value = "シフトを押しながらクリックされました";
+};
+</script>
+
+<template>
+  <p>{{ message }}</p>
+  <input type="text" @keydown.enter="onEnterKeyPress" />
+  <br />
+  <button @click.right.prevent="onRightButtonClick">右クリック</button>
+  <br />
+  <button @click.shift="onClickWithShift">シフトクリック</button>
+  <br />
+</template>

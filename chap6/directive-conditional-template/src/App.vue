8dc3da3c74f2f6465ef159e2743cdb3183f98d21
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-02 08:03:15 +0900

    add project

---
+++ chap6/directive-conditional-template/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const randomNumber = ref(Math.round(Math.random() * 100));
+</script>
+
+<template>
+  <p>
+    点数は{{ randomNumber }}点
+    <template v-if="randomNumber >= 80">
+      で優です。
+      <span class="textRed">素晴らしい！</span>
+    </template>
+  </p>
+</template>
+
+<style>
+.textRed {
+  color: red;
+}
+</style>

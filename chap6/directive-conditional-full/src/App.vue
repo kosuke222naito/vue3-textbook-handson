0f1633e2182fad11aabae4bb2bfd55c13455ea39
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-02 07:52:17 +0900

    add project

---
+++ chap6/directive-conditional-full/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const randomNumber = ref(Math.round(Math.random() * 100));
+</script>
+
+<template>
+  <section>
+    点数は{{ randomNumber }}点で、
+    <span v-if="randomNumber >= 80">優です。</span>
+    <span v-else-if="randomNumber >= 70">良です。</span>
+    <span v-else-if="randomNumber >= 60">可です。</span>
+    <span v-else>不可です。</span>
+  </section>
+</template>

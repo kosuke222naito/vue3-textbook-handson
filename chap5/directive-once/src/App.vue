ed7ba5d6f3cda7a1c6ce23056cfc971402bddea9
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-01 14:40:11 +0900

    add project

---
+++ chap5/directive-once/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const price = ref(100);
+</script>
+
+<template>
+  <section>
+    <input type="number" v-model="price" />円
+    <br />
+    <p>金額は{{ price }}円です。</p>
+    <p v-once>金額は{{ price }}円です。</p>
+  </section>
+</template>

87fc62977ebb720333904a86d46de27978f8cd9e
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-01 15:51:46 +0900

    add project

---
+++ chap6/directive-conditional-basic/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const number = ref(80);
+const isShown = computed((): boolean => {
+  return !Math.round(Math.random());
+});
+</script>
+
+<template>
+  <p v-if="number >= 50">(1): (常に)表示される</p>
+  <p v-if="Math.round(Math.random()) >= 0.5">(2): 表示されることもある</p>
+  <p v-if="isShown">(3): 表示されることもある</p>
+</template>

6303198a2c4295dbdaad5c7ab3dfecd5d3615aea
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-01 10:04:18 +0900

    add project

---
+++ chap5/directive-oneway/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const inputNameBind = ref("しんちゃん");
+const inputNameOn = ref("ななし");
+
+const onInputName = (event: Event): void => {
+  inputNameOn.value = (event.target as HTMLInputElement).value;
+};
+</script>
+
+<template>
+  <section>
+    <input type="text" :value="inputNameBind" />
+  </section>
+  <br />
+  <section>
+    <input type="text" @input="onInputName" />
+    <p>{{ inputNameOn }}</p>
+  </section>
+</template>

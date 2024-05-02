e20565384436f976e0dbe27302660b398cd335ae
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-02 17:15:06 +0900

    add project

---
+++ chap6/directive-loop-range/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="r in 5" :key="r">半径{{ r }}の円の円周: {{ 2 * r * 3.14 }}</li>
+    </ul>
+  </section>
+
+  <section>
+    <select>
+      <option v-for="year in 60" :key="year" :value="year + 1965">{{ year + 1965 }}年</option>
+    </select>
+  </section>
+</template>

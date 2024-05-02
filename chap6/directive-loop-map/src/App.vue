ea194820f4cd7b338db0b758a4e9fe4cc03d73fa
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-02 16:07:21 +0900

    add project

---
+++ chap6/directive-loop-map/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const cocktailsInit = new Map<number, string>();
+cocktailsInit.set(2345, "ホワイトレディ");
+cocktailsInit.set(4412, "ブルーハワイ");
+cocktailsInit.set(6792, "ニューヨーク");
+const cocktails = ref(cocktailsInit);
+</script>
+
+<template>
+  <ul>
+    <li v-for="[id, cocktailName] in cocktails" :key="id">
+      IDが{{ id }}のカクテルは{{ cocktailName }}
+    </li>
+  </ul>
+</template>

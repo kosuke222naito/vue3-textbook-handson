ee94214a0dfe04ca238c1ebc678105c4e7890c45
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-05 01:32:32 +0900

    rewrite to original app

--- chap7/change-cocktail/src/App.vue
+++ chap7/change-cocktail/src/App.vue
@@ -1,47 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { computed, ref } from "vue";
+
+interface Cocktail {
+  id: number;
+  name: string;
+  price: number;
+  recipe?: string;
+}
+
+const cocktailsInit = new Map<number, Cocktail>();
+cocktailsInit.set(1, { id: 1, name: "ホワイトレディ", price: 1200 });
+cocktailsInit.set(2, { id: 2, name: "ブルーハワイ", price: 1500 });
+cocktailsInit.set(3, { id: 3, name: "ニューヨーク", price: 1100 });
+cocktailsInit.set(4, { id: 4, name: "マティーニ", price: 1500 });
+const cocktails = ref(new Map(cocktailsInit));
+
+const currentCocktailId = ref(1);
+const priceMessage = computed((): string => {
+  const currentCocktail = cocktailsInit.get(currentCocktailId.value);
+  if (!currentCocktail) {
+    return "該当カクテルはありません。";
+  }
+  return `該当するカクテルは${currentCocktail.name}で、値段は${currentCocktail.price}円です。`;
+});
+
+setInterval((): void => {
+  currentCocktailId.value = Math.round(Math.random() * 3) + 1;
+}, 1000);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>現在のカクテル番号: {{ currentCocktailId }}</p>
+  <p>{{ priceMessage }}</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>

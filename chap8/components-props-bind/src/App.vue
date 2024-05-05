c543ef35de958b7e253b100a14adb5a59e9ac1e7
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-06 01:49:47 +0900

    add project

---
+++ chap8/components-props-bind/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import OneInfo from "./components/OneInfo.vue";
+
+const propsTitle = ref("発生した乱数");
+const randomNumberInit = Math.round(Math.random() * 100);
+const randomNumber = ref(randomNumberInit);
+</script>
+
+<template>
+  <h1>Props基礎</h1>
+  <section>
+    <h2>テンプレート変数を利用</h2>
+    <OneInfo :title="propsTitle" :content="randomNumber" />
+  </section>
+</template>

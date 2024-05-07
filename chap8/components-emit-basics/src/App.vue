219375397d2394505ea29636efae8d91574ac0d5
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-05-07 18:13:51 +0900

    add project

---
+++ chap8/components-emit-basics/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import OneSection from "./components/OneSection.vue";
+
+const randomNumberInit = Math.round(Math.random() * 10);
+const randomNumber = ref(randomNumberInit);
+
+const updateRandomNumber = (): void => {
+  randomNumber.value = Math.round(Math.random() * 10);
+};
+</script>
+
+<template>
+  <section>
+    <p>親コンポーネントで乱数を表示: {{ randomNumber }}</p>
+    <OneSection :randomNumber @onUpdateRandomNumberButtonClick="updateRandomNumber" />
+  </section>
+</template>
---
+++ chap8/components-emit-basics/src/components/OneSection.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  randomNumber: number;
+}
+
+interface Emits {
+  (event: "onUpdateRandomNumberButtonClick"): void;
+}
+
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+
+const onUpdateRandomNumberButtonClick = (): void => {
+  emit("onUpdateRandomNumberButtonClick");
+};
+</script>
+
+<template>
+  <section class="box">
+    <p>子コンポーネントで乱数を表示: {{ randomNumber }}</p>
+    <button type="button" @click="onUpdateRandomNumberButtonClick">新たな乱数</button>
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
